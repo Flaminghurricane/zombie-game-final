83482e8e5c86f8a606ddcaad87a9c369012682b8
Flaminghurricane <86059679+Flaminghurricane@users.noreply.github.com>
2021-10-18 18:08:24 +0530

    Update Form.js

--- Form.js
+++ Form.js
@@ -18,7 +18,8 @@ class Form
       //MAke this name of the game
       var title=createElement("h2")
       title.html("It has been 3 weeks since the zombie virus has spread.Your goal is to escape the zombie apocalipse.Press the right and left arrow to move and press the left mouse button to shoot.There are 3 levels in this game.In the first level you have 6 lives.Your life decreases when a zombie touches your car.To progress to the second level you will need to obtain 25 points.The score increases when you kill a zombie.In the second level you will have 3 lives and will need 35 points to progress to thefinal level.In the final level you will have 1 life and will need 40 points to win the game.click the 'next'button to start the game.GOOD LUCK SURVIVOR.")
-      title.position(100,150)
+      title.position(100,150); 
+      title.class("title_color");
       var btn1=createButton("NEXT") 
       
       
